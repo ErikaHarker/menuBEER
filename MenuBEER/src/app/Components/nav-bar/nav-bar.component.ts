8fe6239904f47fd144bd7de6c9403006f7706f3b
ErikaHarker <harkerika@gmail.com>
2019-02-08 12:39:00 -0500

    Correction structure

--- MenuBEER/src/app/Components/nav-bar/nav-bar.component.ts
+++ MenuBEER/src/app/Components/nav-bar/nav-bar.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  public isLogin: Boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
+  
 
 }
